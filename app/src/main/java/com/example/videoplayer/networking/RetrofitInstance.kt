7ccae511c975f157d93a0f1be1a80d41d769012f
vishnurahar1 <vishnurahar1@gmail.com>
2022-07-06 20:57:42 +0530

    Api auth fix

--- app/src/main/java/com/example/videoplayer/networking/RetrofitInstance.kt
+++ app/src/main/java/com/example/videoplayer/networking/RetrofitInstance.kt
@@ -4,7 +4,6 @@ import com.example.videoplayer.util.Constants.Companion.API_KEY
 import com.example.videoplayer.util.Constants.Companion.BASE_URL
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -13,9 +12,20 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
 
     fun getInstance(): Retrofit {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(Interceptor { chain ->
+                val request = chain.request().newBuilder().addHeader("Authorization", API_KEY).build()
+                chain.proceed(request)
+            })
+            .addInterceptor(logging)
+            .build()
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
     }
 }
